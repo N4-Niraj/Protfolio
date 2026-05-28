a3291373119b707afc8b2734e95d994fe4e2b49d
N4-Niraj <bhusalniraj03@gmail.com>
2026-05-29 03:07:14 +0545

    update: nav and hero section fixed

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    nav.classList.add('scrolled');
+  } else {
+    nav.classList.remove('scrolled');
+  }
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    
+    if (target) {
+      const navHeight = document.querySelector('nav').offsetHeight;
+      const targetPosition = target.offsetTop - navHeight;
+      
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth"
+      });
+    }
+  });
+});
